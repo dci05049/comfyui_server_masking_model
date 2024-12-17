ffc070882e7fb83daf4a38f2aeeddb80bcc03c02
jasonjunggil <jyjason.junggil@gmail.com>
2024-12-17 17:03:14 -0500

    add workflow to create shirts on model / inpaint logo onto shirts

--- comfyuiservice.py
+++ comfyuiservice.py
@@ -1,6 +1,7 @@
 import websocket 
 import uuid
 import json
+import os
 import urllib.request
 import urllib.parse
 
@@ -9,235 +10,66 @@ server_address = "127.0.0.1:8188"
 client_id = str(uuid.uuid4())
 
 
-def get_prompt_with_workflow(input):
-    prompt_text = '''
-    {
-    "6": {
-        "inputs": {
-        "text": "",
-        "clip": [
-            "38",
-            1
-        ]
-        },
-        "class_type": "CLIPTextEncode",
-        "_meta": {
-        "title": "CLIP Text Encode (Positive Prompt)"
-        }
-    },
-    "8": {
-        "inputs": {
-        "samples": [
-            "13",
-            0
-        ],
-        "vae": [
-            "10",
-            0
-        ]
-        },
-        "class_type": "VAEDecode",
-        "_meta": {
-        "title": "VAE Decode"
-        }
-    },
-    "9": {
-        "inputs": {
-        "filename_prefix": "ComfyUI",
-        "images": [
-            "8",
-            0
-        ]
-        },
-        "class_type": "SaveImage",
-        "_meta": {
-        "title": "Save Image"
-        }
-    },
-    "10": {
-        "inputs": {
-        "vae_name": "ae.safetensors"
-        },
-        "class_type": "VAELoader",
-        "_meta": {
-        "title": "Load VAE"
-        }
-    },
-    "11": {
-        "inputs": {
-        "clip_name1": "t5xxl_fp16.safetensors",
-        "clip_name2": "clip_l.safetensors",
-        "type": "flux"
-        },
-        "class_type": "DualCLIPLoader",
-        "_meta": {
-        "title": "DualCLIPLoader"
-        }
-    },
-    "12": {
-        "inputs": {
-        "unet_name": "flux_dev.safetensors",
-        "weight_dtype": "default"
-        },
-        "class_type": "UNETLoader",
-        "_meta": {
-        "title": "Load Diffusion Model"
-        }
-    },
-    "13": {
-        "inputs": {
-        "noise": [
-            "25",
-            0
-        ],
-        "guider": [
-            "22",
-            0
-        ],
-        "sampler": [
-            "16",
-            0
-        ],
-        "sigmas": [
-            "17",
-            0
-        ],
-        "latent_image": [
-            "27",
-            0
-        ]
-        },
-        "class_type": "SamplerCustomAdvanced",
-        "_meta": {
-        "title": "SamplerCustomAdvanced"
-        }
-    },
-    "16": {
-        "inputs": {
-        "sampler_name": "euler"
-        },
-        "class_type": "KSamplerSelect",
-        "_meta": {
-        "title": "KSamplerSelect"
-        }
-    },
-    "17": {
-        "inputs": {
-        "scheduler": "simple",
-        "steps": 20,
-        "denoise": 1,
-        "model": [
-            "30",
-            0
-        ]
-        },
-        "class_type": "BasicScheduler",
-        "_meta": {
-        "title": "BasicScheduler"
-        }
-    },
-    "22": {
-        "inputs": {
-        "model": [
-            "38",
-            0
-        ],
-        "conditioning": [
-            "26",
-            0
-        ]
-        },
-        "class_type": "BasicGuider",
-        "_meta": {
-        "title": "BasicGuider"
-        }
-    },
-    "25": {
-        "inputs": {
-        "noise_seed": 1234511
-        },
-        "class_type": "RandomNoise",
-        "_meta": {
-        "title": "RandomNoise"
-        }
-    },
-    "26": {
-        "inputs": {
-        "guidance": 3.5,
-        "conditioning": [
-            "6",
-            0
-        ]
-        },
-        "class_type": "FluxGuidance",
-        "_meta": {
-        "title": "FluxGuidance"
-        }
-    },
-    "27": {
-        "inputs": {
-        "width": 1024,
-        "height": 1024,
-        "batch_size": 1
-        },
-        "class_type": "EmptySD3LatentImage",
-        "_meta": {
-        "title": "EmptySD3LatentImage"
-        }
-    },
-    "30": {
-        "inputs": {
-        "max_shift": 1.15,
-        "base_shift": 0.5,
-        "width": 1024,
-        "height": 1024,
-        "model": [
-            "12",
-            0
-        ]
-        },
-        "class_type": "ModelSamplingFlux",
-        "_meta": {
-        "title": "ModelSamplingFlux"
-        }
-    },
-    "38": {
-        "inputs": {
-        "lora_name": "flux_realism_lora.safetensors",
-        "strength_model": 1,
-        "strength_clip": 1,
-        "model": [
-            "30",
-            0
-        ],
-        "clip": [
-            "11",
-            0
-        ]
-        },
-        "class_type": "LoraLoader",
-        "_meta": {
-        "title": "Load LoRA"
-        }
-    },
-    "40": {
-        "inputs": {
-        "images": [
-            "8",
-            0
-        ]
-        },
-        "class_type": "SaveImageWebsocket",
-        "_meta": {
-        "title": "SaveImageWebsocket"
-        }
-    }
-    }
-    '''
+def get_inpaint_image_on_target_json(inpaint_image_base64, target_image_base64, mask_image_base64):
+    TARGETIMAGEID = "410"
+    INPAINTIMAGEID = "412"
+    MASKIMAGEID = "393"
 
-    prompt_json = json.loads(prompt_text)
-    prompt_json["6"]["inputs"]["text"] = input
-    return prompt_json
+    file_path = "data.json"
+
+    workflow_folder = "workflows-api"
+    file_name = "flux-redux-cloth-swap.json"
+
+    # Construct the full path to the JSON file
+    file_path = os.path.join(workflow_folder, file_name)
+
+    # Open the JSON file and load its content into a variable
+    with open(file_path, "r") as file:
+        prompt_json = json.load(file)
+        prompt_json[TARGETIMAGEID]["inputs"]["image"] = target_image_base64
+        prompt_json[INPAINTIMAGEID]["inputs"]["image"] = inpaint_image_base64
+        prompt_json[MASKIMAGEID]["inputs"]["mask"] = mask_image_base64
+        return prompt_json
+    
+def get_cloth_with_logo_image_json(cloth_base_64, logo_image_base64, mask_base_64):
+    CLOTHIMAGEID = "410"
+    MODELIMAGEID = "412"
+    MASKIMAGEID = "393"
+
+    file_path = "data.json"
+
+    workflow_folder = "workflows-api"
+    file_name = "flux-redux-cloth-swap.json"
+
+    # Construct the full path to the JSON file
+    file_path = os.path.join(workflow_folder, file_name)
+
+    # Open the JSON file and load its content into a variable
+    with open(file_path, "r") as file:
+        prompt_json = json.load(file)
+        prompt_json[CLOTHIMAGEID]["inputs"]["image"] = logo_image_base64
+        prompt_json[MODELIMAGEID]["inputs"]["image"] = cloth_base_64
+        prompt_json[MASKIMAGEID]["inputs"]["mask"] = mask_base_64
+        return prompt_json
+    
+
+def get_cloth_with_prompt(prompt):
+    TEXTID = "6"
+
+    file_path = "data.json"
+
+    workflow_folder = "workflows-api"
+    file_name = "flux-guff-text-api.json"
+
+    # Construct the full path to the JSON file
+    file_path = os.path.join(workflow_folder, file_name)
+
+    # Open the JSON file and load its content into a variable
+    with open(file_path, "r") as file:
+        prompt_json = json.load(file)
+        prompt_json[TEXTID]["inputs"]["text"] = prompt
+        return prompt_json
+
 
 def queue_prompt(prompt):
     p = {"prompt": prompt, "client_id": client_id}
@@ -245,15 +77,7 @@ def queue_prompt(prompt):
     req =  urllib.request.Request("http://{}/prompt".format(server_address), data=data)
     return json.loads(urllib.request.urlopen(req).read())
 
-def get_image(filename, subfolder, folder_type):
-    data = {"filename": filename, "subfolder": subfolder, "type": folder_type}
-    url_values = urllib.parse.urlencode(data)
-    with urllib.request.urlopen("http://{}/view?{}".format(server_address, url_values)) as response:
-        return response.read()
 
-def get_history(prompt_id):
-    with urllib.request.urlopen("http://{}/history/{}".format(server_address, prompt_id)) as response:
-        return json.loads(response.read())
 
 def get_images(ws, prompt):
     prompt_id = queue_prompt(prompt)['prompt_id']
@@ -280,6 +104,24 @@ def get_images(ws, prompt):
 def fetch_image_from_comfy(input):
     ws = websocket.WebSocket()
     ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
-    images = get_images(ws, get_prompt_with_workflow(input))
+    images = get_images(ws, get_cloth_with_prompt(input))
+    ws.close()
+    return images
+
+def swap_clothes_on_model(cloth_base_64, model_base_64, mask_base_64):
+    ws = websocket.WebSocket()
+    ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
+    images = get_images(ws, get_inpaint_image_on_target_json(inpaint_image_base64=cloth_base_64, target_image_base64=model_base_64, mask_image_base64=mask_base_64))
+
+    print(images)
+    ws.close()
+    return images
+
+def get_cloth_with_logo(cloth_base64, logo_image_base64, mask_image_base64):
+    ws = websocket.WebSocket()
+    ws.connect("ws://{}/ws?clientId={}".format(server_address, client_id))
+    images = get_images(ws, get_inpaint_image_on_target_json(inpaint_image_base64=logo_image_base64, target_image_base64=cloth_base64, mask_image_base64=mask_image_base64))
+
+    print(images)
     ws.close()
     return images
--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 from fastapi import FastAPI, Query
+from fastapi import File, UploadFile, Form
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+from io import BytesIO
+import base64
 import io
 import comfyuiservice
 
 app = FastAPI()
+
+async def convert_image_to_base64(image_file):
+    """
+    Converts an uploaded image file to a Base64 string.
+    """
+    # Open the image from the uploaded file
+    img = Image.open(BytesIO(await image_file.read()))
+    
+    # Save the image to a BytesIO object in the desired format
+    img_byte_arr = BytesIO()
+    img.save(img_byte_arr, format="PNG")
+    img_byte_arr.seek(0)  # Reset stream pointer to the beginning
+
+    # Encode the BytesIO content to Base64
+    base64_encoded_image = base64.b64encode(img_byte_arr.getvalue()).decode("utf-8")
+    return base64_encoded_image
+
+async def save_base64_to_file(base64_data, filename):
+    """
+    Save a Base64 string to a text file.
+    """
+    with open(filename, "w") as file:
+        file.write(base64_data)
 
 # Add CORS middleware
 app.add_middleware(
@@ -21,6 +48,75 @@ async def get_image(input: str = Query(None)):
     image_stream = io.BytesIO(image)
     return StreamingResponse(image_stream, media_type="image/png")
 
+
+@app.post("/get_model_with_image/")
+async def get_model_with_image(
+    mask_image: UploadFile = File(..., description="Mask image (JPG or PNG)"),
+    person_image: UploadFile = File(..., description="Person image (JPG or PNG)"),
+    clothing_image: UploadFile = File(..., description="Clothing image (JPG or PNG)"),
+):
+    """
+    Endpoint to process three images and return modified images as raw data.
+    """
+    # Load images into memory
+    mask_base64 = await convert_image_to_base64(mask_image)
+    person_base64 = await convert_image_to_base64(person_image)
+    cloth_base64 = await convert_image_to_base64(clothing_image)
+
+    result_image = comfyuiservice.swap_clothes_on_model(cloth_base64, person_base64, mask_base64)
+    image_stream = io.BytesIO(result_image)
+    return StreamingResponse(image_stream, media_type="image/png")
+
+@app.post("/get_model_with_prompt/")
+async def get_model_with_prompt(
+   input: str = Query(None)
+):
+    """
+    Endpoint to process three images and return modified images as raw data.
+    """
+    # Load images into memory
+    """
+    Endpoint to process three images and return modified images as raw data.
+    """
+    # Load images into memory
+    image = comfyuiservice.fetch_image_from_comfy(input)
+    image_stream = io.BytesIO(image)
+    return StreamingResponse(image_stream, media_type="image/png")
+
+
+@app.get("/get_cloth_with_prompt/")
+async def get_cloth_with_prompt(
+    input: str = Query(None)
+):
+    """
+    Endpoint to process three images and return modified images as raw data.
+    """
+    # Load images into memory
+    image = comfyuiservice.fetch_image_from_comfy(input)
+    image_stream = io.BytesIO(image)
+    return StreamingResponse(image_stream, media_type="image/png")
+
+@app.post("/get_cloth_with_logo/")
+async def get_cloth_with_logo(
+    mask_image: UploadFile = File(..., description="Mask image (JPG or PNG)"),
+    clothing_image: UploadFile = File(..., description="Cloth image (JPG or PNG)"),
+    logo_image: UploadFile = File(..., description="Logo image (JPG or PNG)"),
+):
+    """
+    Endpoint to process three images and return modified images as raw data.
+    """
+    # Load images into memory
+    mask_base64 = await convert_image_to_base64(mask_image)
+    logo_base64 = await convert_image_to_base64(logo_image)
+    cloth_base64 = await convert_image_to_base64(clothing_image)
+
+    result_image = comfyuiservice.get_cloth_with_logo(cloth_base64=cloth_base64, logo_image_base64=logo_base64, mask_image_base64=mask_base64)
+    image_stream = io.BytesIO(result_image)
+    return StreamingResponse(image_stream, media_type="image/png")
+
 @app.get("/hello")
 def read_hello():
     return {"message": "Hello World!"}
+
+
+
